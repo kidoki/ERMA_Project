7afa5885f69a038f4755a023059ec9d874dbb06b
ParkKiDeok <kidoki@naver.com>
2016-08-15 19:16:20 +0900

    NoteBook Commit Test

--- src/main/java/com/erma/main/controller/MainController.java
+++ src/main/java/com/erma/main/controller/MainController.java
@@ -40,7 +40,7 @@ public class MainController {
         Map<String,Object> map = new HashMap<String,Object>();
         ResponseVO response = new ResponseVO();
         
-        log.debug("아파트 가격 조회 Test");
+        log.debug("아파트 가격 조회 Test01");
         
         if(commandMap.isEmpty() == false){
         	map = commandMap.getMap(); 
